a7018f023e65eddbb42a89297acafe8bacf47d74
Sloth247 <86727485+Sloth247@users.noreply.github.com>
2021-07-08 21:31:06 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 'use strict';
-erySelector('.warning');
-const email = document.q;
-const warning = document.quuerySelector('.email');
+const email = document.querySelector('.email');
+const errorMessage = document.querySelector('.warning');
 const selectedForm = document.getElementById('form');
 
 selectedForm.addEventListener('submit', function (event) {
   event.preventDefault();
   if (email.validity.typeMismatch || email.validity.valueMissing) {
-    warning.classList.remove('hidden');
+    errorMessage.classList.remove('hidden');
   } else {
     email.setCustomValidity('');
   }
 });
 
 email.addEventListener('input', function () {
-  warning.classList.add('hidden');
+  errorMessage.classList.add('hidden');
 });
